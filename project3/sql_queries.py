1006d75560c5d057cd685d795c13e92619f5d634
Chandrasekhar Reddy Muthyala <cmuth001@odu.edu>
2019-06-08 21:03:40 -0400

    Created staging_events_table and staging_songs_table query

--- project3/sql_queries.py
+++ project3/sql_queries.py
@@ -7,20 +7,50 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events_table"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs_table"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay_table"
+user_table_drop = "DROP TABLE IF EXISTS user_table"
+song_table_drop = "DROP TABLE IF EXISTS song_table"
+artist_table_drop = "DROP TABLE IF EXISTS artist_table"
+time_table_drop = "DROP TABLE IF EXISTS time_table"
 
 # CREATE TABLES
 
-staging_events_table_create= ("""
+staging_events_table_create= ("""CREATE TABLE IF NOT EXISTS staging_events(
+                                artist VARCHAR(MAX),
+                                auth VARCHAR(25),
+                                firstName VARCHAR(MAX),
+                                gender CHAR(1),
+                                itemSession INTEGER,
+                                lastName VARCHAR(MAX),
+                                length FLOAT4,
+                                level VARCHAR(5),
+                                location VARCHAR(MAX),
+                                method VARCHAR(4),
+                                page VARCHAR(12),
+                                registration FLOAT8,
+                                sessionId INTEGER,
+                                song VARCHAR(MAX),
+                                status INTEGER,
+                                ts VARCHAR(MAX),
+                                userAgent VARCHAR(MAX),
+                                userId INTEGER
+                            )
 """)
 
-staging_songs_table_create = ("""
+staging_songs_table_create = ("""CREATE  TABLE IF NOT EXISTS staging_songs(
+                                num_songs INTEGER,
+                                artistId VARCHAR(MAX),
+                                artistLatitude FLOAT8,
+                                artistLongitude FLOAT8,
+                                artistLocation VARCHAR(MAX),
+                                artistName VARCHAR(MAX),
+                                songId VARCHAR(MAX),
+                                title VARCHAR(MAX),
+                                duration FLOAT4,
+                                year INTEGER
+                            )
 """)
 
 songplay_table_create = ("""
