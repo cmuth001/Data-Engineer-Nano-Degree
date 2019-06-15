05b2e04ae87bf39266e43efefe6a78b281dbdf47
cmuth001 <cmuth001@odu.edu>
2019-06-15 00:22:41 -0400

    power of spark demo file

--- project3/create_tables.py
+++ project3/create_tables.py
@@ -2,7 +2,6 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
@@ -11,14 +10,16 @@ def drop_tables(cur, conn):
 
 def create_tables(cur, conn):
     for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+        try:
+            cur.execute(query)
+            conn.commit()
+        except Exception as e:
+            print(e)
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
